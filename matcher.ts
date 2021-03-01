ac967712daf4cfd81f309930094c5b18de626f28
Serena Wang <serena.happyhacking@gmail.com>
2021-03-01 10:04:14 +0800

    feat:xx

---
+++ matcher.ts
@@ -0,0 +1,10 @@
+export function createMatcher(
+  routes: Array<RouteConfig>,
+  router:Vuerouter
+): Matcher {
+  const { pathList, pathMap, nameMap }  = createRouteMap(routers)
+
+  function addRoutes(routes) {
+    
+  }
+}
