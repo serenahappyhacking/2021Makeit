72006468cc6b88635a621a612f89515ad327f01a
Serena <serena.happyhacking@gmail.com>
2021-03-03 17:50:54 +0800

    feat:重构目录

---
+++ principles/webpack打包原理.md
@@ -0,0 +1,1 @@
+###### https://juejin.cn/post/6844904038543130637#heading-7
--- vue-loader.js
+++
@@ -1,6 +0,0 @@
-/**
- *  vue loader is a loader for webpack , transform *.vue to *.bundle.js . 然后放入浏览器中运行。
- *  就是把 vue 组件（vue 文件） 转换为一个 a plain js module.
- *
- *  vue-loader 把 *.vue 转化为 js 模块的时候，其实内部调用了 4 个 小的 loader: selector,  style-compiler, template-compiler, babel-loader.
- */
