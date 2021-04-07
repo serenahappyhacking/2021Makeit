89291fb6aa4bfdecf5065def3f90ea52bd39b3b8
Serena <serena.happyhacking@gmail.com>
2021-04-07 18:28:59 +0800

    feat:s

--- 手写js/promise 系列/promise_all.js
+++ 手写js/promise 系列/promise_all.js
@@ -1,20 +1,22 @@
 function PromiseAll(arr) {
-	return new Promise((resolve, reject) => {
-		if (!Array.isArray(arr)) {
-			return reject(new Error('传入的参数必须是数组！'))
-		}
-		let result = []
-		let counter = 0 // 计数器
-		for (let i = 0; i < arr.length; i++) {
-			Promise.resolve(arr[i])
-				.then((value) => {
-					counter++
-					result[i] = value // 这一步是为了顺序输出所有的 promise
-					if (counter === arr.length) {
-						resolve(result)
-					}
-				})
-				.catch((reason) => reject(reason))
-		}
-	})
+  return new Promise((resolve, reject) => {
+    if (!Array.isArray(arr)) {
+      return reject(new Error('传入的参数必须是数组！'))
+    }
+    let result = []
+    let counter = 0 // 计数器
+    for (let i = 0; i < arr.length; i++) {
+      Promise.resolve(arr[i])
+        .then((value) => {
+          counter++
+          result[i] = value // 这一步是为了顺序输出所有的 promise
+          if (counter === arr.length) {
+            resolve(result)
+          }
+        })
+        .catch((reason) => reject(reason))
+    }
+  })
 }
+
+Promise.all([p1, p2]).then(() => {})
