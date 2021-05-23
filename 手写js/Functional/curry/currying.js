d9411313b04c5f10306be142c7a87427f99d3b1a
Serena Wang <serena.happyhacking@gmail.com>
2021-05-23 14:43:13 +0800

    s

--- 手写js/Functional/curry/currying.js
+++ 手写js/Functional/curry/currying.js
@@ -1,4 +1,16 @@
-// 定长参数
+/* curry 是一种 transform of function， 把一个多个入参的函数调用 f(a,b,c)，转换成 f(a)(b)(c). 
+		让一个函数的执行可控，可以只先执行部分的逻辑： “partially applied function” or “partial” for short.
+
+		比如打印几个参数 log(new Date(), 'Debug', 'some others'), 
+		curry transform 后变成了 logCurry(new Date())('Debug')('some others').
+
+		这样把参数分开传递，就可以一部分一部分的执行这个方法：
+		const log1 = logCurry(new Date())
+		const log2 = log1('Debug')
+		const log3 = log2('some others')
+*/
+
+// curry 定长参数
 const add = (a, b, c) => a + b + c
 
 const curry = (fn) =>
@@ -73,7 +85,7 @@ console.log(
 
 // args with arrow function 该怎么使用
 const fn1 = (...args) => {
-	const [a, b, c] = args
+	const [a, b, c] = args // 解构 paramters deconstruction
 	console.log('>>>', ...args)
 	console.log('***', a, b, c)
 	return a + b + c
